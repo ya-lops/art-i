9076ecb18e0dc1de3ea86da125e6bfd8690aa15c
Lapsar Vitaliy <Lapsar.VM@gazprom-neft.ru>
2024-01-25 10:38:59 +0300

    upd config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ya-lops.github.io',
+  site: 'https://ya-lops.github.io/art-i',
 });
