3c7f6ef39b3aec61a934f5368de4d12f04102dda
Lapsar Vitaliy <Lapsar.VM@gazprom-neft.ru>
2024-01-25 10:44:19 +0300

    add base

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ya-lops.github.io/art-i',
+  site: 'https://ya-lops.github.io',
+  base: '/art-i',
 });
